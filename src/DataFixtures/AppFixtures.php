57ef3cfea3b76a28507750ed0a8b51384ba0ec9c
Ernest Mazur <emazemhs@gmail.com>
2021-08-16 15:53:40 +0200

    AUTOCOMMIT: 16-08-2021_15:53:40

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Auction;
 use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -18,6 +19,7 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $this->loadUsers($manager);
+        $this->loadAuctions($manager);
     }
 
     private function loadUsers(ObjectManager $manager) : void
@@ -30,6 +32,7 @@ class AppFixtures extends Fixture
             $user->setPassword($this->passwordHasher->hashPassword($user, $password));
             $user->setRoles($roles);
             $manager->persist($user);
+            $this->tempUser = $user;
         }
 
         $manager->flush();
@@ -39,6 +42,34 @@ class AppFixtures extends Fixture
     {
         return[
             ['Emis', 'jahael@gmail.com', '12345', ['ROLE_ADMIN']]
+        ];
+    }
+
+
+    private function loadAuctions(ObjectManager $manager) : void
+    {
+        foreach ($this->getAuctionData() as [$title,  $createdAt, $description, $roles])
+        {
+            $auction = new Auction();
+            $auction->setByUser($this->tempUser);
+            $auction->setTitle($title);
+            $auction->setCreatedAt($createdAt);
+            $auction->setEndsAt($createdAt->modify('+1 day'));
+            $auction->setDescription($description);
+            $user->setUsername($username);
+            $user->setEmail($email);
+            $user->setPassword($this->passwordHasher->hashPassword($user, $password));
+            $user->setRoles($roles);
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+    }
+
+    public function getAuctionData(): array
+    {
+        return[
+            ['Piec wendzarzniczy BOLOKS', 'jahael@gmail.com', '12345', ['ROLE_ADMIN']]
         ];
     }
 }
