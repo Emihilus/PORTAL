da5a591021f55c627a11cc9868d05c8e3900765b
Ernest Mazur <emazemhs@gmail.com>
2021-08-18 02:51:43 +0200

    AUTOCOMMIT: 18-08-2021_02:51:43

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -27,13 +27,13 @@ class MainController extends AbstractController
         $auctions = $this->getDoctrine()->getRepository(Auction::class)->findAll();
         $allCount = count($auctions);
 
-        $auctions = $this->paginator->paginate($auctions,$page, Auction::perPage);
-        
+        $auctions = $this->paginator->paginate($auctions, $page, Auction::perPage);
+
 
 
         return $this->render('main/auction_list.html.twig', [
             'auctions' => $auctions,
-            'pages' => $allCount%Auction::perPage === 0 ? $allCount/Auction::perPage : intval($allCount/Auction::perPage)+1
+            'pages' => $allCount % Auction::perPage === 0 ? $allCount / Auction::perPage : intval($allCount / Auction::perPage) + 1
         ]);
     }
 
@@ -49,18 +49,21 @@ class MainController extends AbstractController
         ]);
     }
 
-     /**
+    /**
      * @Route("/ep/setPerPage", name="setPerPage", methods={"POST"})
      */
     public function setPerPageCookie(Request $request)
     {
-        $postData = json_decode($request->getContent());
-        
+        $parametersAsArray = [];
+        if ($content = $request->getContent()) {
+            $parametersAsArray = json_decode($content, true);
+        }
+
         //$result = $postData['itemsPerPage'];
 
         return new JsonResponse([
             'RESPONSE' => 'resp',
-            'itemsPerPage' => $postData
+            'itemsPerPage' => $parametersAsArray['itemsPerPage']
         ]);
     }
 }
