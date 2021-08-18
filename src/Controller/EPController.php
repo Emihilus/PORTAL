f938cc2c122654d04f31ee40aef26a0faf99fbc3
Ernest Mazur <emazemhs@gmail.com>
2021-08-18 16:44:29 +0200

    AUTOCOMMIT: 18-08-2021_16:44:29

--- src/Controller/EPController.php
+++ src/Controller/EPController.php
@@ -12,7 +12,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\SerializerInterface;
 
-class MainController extends AbstractController
+class EPController extends AbstractController
 {
     public function __construct(PaginatorInterface $paginator)
     {
