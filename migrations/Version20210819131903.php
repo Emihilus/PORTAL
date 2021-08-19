55d379d735ee17ba40ce4dd83fdb4dc717647b2b
Ernest Mazur <emazemhs@gmail.com>
2021-08-19 15:26:07 +0200

    AUTOCOMMIT: 19-08-2021_15:26:07

--- migrations/Version20210819131903.php
+++ migrations/Version20210819131903.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210819012718 extends AbstractMigration
+final class Version20210819131903 extends AbstractMigration
 {
     public function getDescription(): string
     {
