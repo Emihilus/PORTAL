82e0587054b6879e02354b97698438f4ada20e0f
Ernest Mazur <emazemhs@gmail.com>
2021-08-21 21:59:09 +0200

    AUTOCOMMIT: 21-08-2021_21:59:09

--- src/Controller/EndPointsController.php
+++ src/Controller/EndPointsController.php
@@ -36,7 +36,7 @@ class EndPointsController extends AbstractController
      */
     public function getAuctions(Request $request)
     {
-        $auctions = $this->getDoctrine()->getRepository(Auction::class)->findAllWithHighestOfferValue();
+        $auctions = $this->getDoctrine()->getRepository(Auction::class)->findAllWithFirstImageAndHighestOffer();
         // dump();
 
         $_POST_requestedPage = $request->get('requestedPage');
