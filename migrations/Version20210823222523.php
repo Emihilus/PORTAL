4c9d65cef1a3b03a0c5187677c44d86c99b88213
Ernest Mazur <emazemhs@gmail.com>
2021-08-24 00:31:45 +0200

    AUTOCOMMIT: 24-08-2021_00:31:45

--- migrations/Version20210823222523.php
+++ migrations/Version20210823222523.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210823135359 extends AbstractMigration
+final class Version20210823222523 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -21,7 +21,7 @@ final class Version20210823135359 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE TABLE auction_images (id INT AUTO_INCREMENT NOT NULL, auction_id INT DEFAULT NULL, filename VARCHAR(255) NOT NULL, order_indicator INT NOT NULL, INDEX IDX_998C125757B8F0DE (auction_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE auctions (id INT AUTO_INCREMENT NOT NULL, by_user_id INT NOT NULL, image_name VARCHAR(255) DEFAULT NULL, title VARCHAR(30) NOT NULL, created_at DATETIME NOT NULL, ends_at DATETIME NOT NULL, description LONGTEXT DEFAULT NULL, INDEX IDX_72D6E900DC9C2434 (by_user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE auctions (id INT AUTO_INCREMENT NOT NULL, by_user_id INT NOT NULL, title VARCHAR(30) NOT NULL, created_at DATETIME NOT NULL, ends_at DATETIME NOT NULL, description TEXT DEFAULT NULL, INDEX IDX_72D6E900DC9C2434 (by_user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE offers (id INT AUTO_INCREMENT NOT NULL, auction_id INT NOT NULL, by_user_id INT NOT NULL, value INT NOT NULL, created_at DATETIME NOT NULL, INDEX IDX_DA46042757B8F0DE (auction_id), INDEX IDX_DA460427DC9C2434 (by_user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE temp_images (id INT AUTO_INCREMENT NOT NULL, token VARCHAR(255) NOT NULL, filename VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE users (id INT AUTO_INCREMENT NOT NULL, username VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_1483A5E9F85E0677 (username), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
--- src/Entity/Auction.php
+++ src/Entity/Auction.php
@@ -7,13 +7,11 @@ use App\Repository\AuctionRepository;
 use Doctrine\Common\Collections\Collection;
 use Symfony\Component\HttpFoundation\File\File;
 use Doctrine\Common\Collections\ArrayCollection;
-use Vich\UploaderBundle\Mapping\Annotation as Vich;
 use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Table(name="auctions")
  * @ORM\Entity(repositoryClass=AuctionRepository::class)
- * @Vich\Uploadable
  */
 class Auction
 {
@@ -25,23 +23,6 @@ class Auction
     private $id;
 
 
-
-    /**
-     * NOTE: This is not a mapped field of entity metadata, just a simple property.
-     * 
-     * @Vich\UploadableField(mapping="images", fileNameProperty="imageName")
-     * 
-     * @var File|null
-     */
-    private $imageFile;
-
-
-    /**
-     * @ORM\Column(type="string", nullable=true)
-     *
-     * @var string|null
-     */
-    private $imageName;
 
     /**
      * @ORM\Column(type="string", length=30)
@@ -59,7 +40,7 @@ class Auction
     private $endsAt;
 
     /**
-     * @ORM\Column(type="text", nullable=true)
+     * @ORM\Column(type="text", nullable=true, length=2000)
      */
     private $description;
 
@@ -87,43 +68,6 @@ class Auction
         $this->offers = new ArrayCollection();
         
     }
-
-    /** 
-    * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
-    * of 'UploadedFile' is injected into this setter to trigger the update. If this
-    * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
-    * must be able to accept an instance of 'File' as the bundle will inject one here
-    * during Doctrine hydration.
-    *
-    * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile|null $imageFile
-    */
-   public function setImageFile(?File $imageFile = null): void
-   {
-       $this->imageFile = $imageFile;
-
-       if (null !== $imageFile) {
-           // It is required that at least one field changes if you are using doctrine
-           // otherwise the event listeners won't be called and the file is lost
-           $this->updatedAt = new \DateTimeImmutable();
-       }
-   }
-
-   public function getImageFile(): ?File
-   {
-       return $this->imageFile;
-   }
-
-   public function setImageName(?string $imageName): void
-   {
-       $this->imageName = $imageName;
-   }
-
-   public function getImageName(): ?string
-   {
-       return $this->imageName;
-   }
-
-
 
 
     public function getId(): ?int
