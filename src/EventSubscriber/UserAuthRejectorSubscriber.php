1b0679d63f04471d51631c40d18ad749b529ecf9
Ernest Mazur <emazemhs@gmail.com>
2021-08-27 02:18:29 +0200

    AUTOCOMMIT: 27-08-2021_02:18:29

--- src/EventSubscriber/UserAuthRejectorSubscriber.php
+++ src/EventSubscriber/UserAuthRejectorSubscriber.php
@@ -7,7 +7,7 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Security\Http\Authenticator\Passport\UserPassportInterface;
 use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
 
-class UserNotVerifiedRejectorSubscriber implements EventSubscriberInterface
+class UserAuthRejectorSubscriber implements EventSubscriberInterface
 {
     public function onCheckPassportEvent(CheckPassportEvent $event)
     {
@@ -19,6 +19,9 @@ class UserNotVerifiedRejectorSubscriber implements EventSubscriberInterface
         
         if(!$user->isVerified())
             throw new CustomUserMessageAccountStatusException('Your user account is not verified.');
+
+        if($user->isBanned())
+            throw new CustomUserMessageAccountStatusException('You have been banned.');
     }
 
     public static function getSubscribedEvents()
