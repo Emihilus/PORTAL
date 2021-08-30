dee221b9a295a99a201c88f5ffebbf2205ba8a16
Ernest Mazur <emazemhs@gmail.com>
2021-08-30 15:44:44 +0200

    AUTOCOMMIT: 30-08-2021_15:44:44

--- src/Repository/AuctionRepository.php
+++ src/Repository/AuctionRepository.php
@@ -262,4 +262,23 @@ class AuctionRepository extends ServiceEntityRepository
 
        return $query;
     }
+
+
+    public function findUserprofileInfoCollection($value)
+    {
+       return $this->createQueryBuilder('a')
+            ->addSelect('('.$this->createQueryBuilder('b')
+            ->select('MAX(o.Value)')
+            ->from('App\Entity\Offer', 'o')
+            ->where('a.id = o.auction')
+            ->getDQL(). ') as hghst')
+
+            ->leftJoin('a.images', 'i')
+            ->addSelect('i.filename')
+            ->where('i.orderIndicator = 0')
+            ->orWhere('i.orderIndicator IS NULL')
+            ->getQuery()
+            ->getResult()
+        ;
+    }
 }
