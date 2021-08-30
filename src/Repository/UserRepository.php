0742db596c817e289fa1381d1476cb416a91aab6
Ernest Mazur <emazemhs@gmail.com>
2021-08-30 17:15:40 +0200

    AUTOCOMMIT: 30-08-2021_17:15:40

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -78,9 +78,14 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
         //return $em->createQuery('SELECT a, i FROM auction a LEFT JOIN a.auction_image i')->getResult();
     }
 
+
+    /*SELECT u0_.id AS id_0, u0_.username AS username_1, u0_.roles AS roles_2, u0_.password AS password_3, u0_.email AS email_4, u0_.is_verified AS is_verified_5, u0_.is_banned AS is_banned_6, (SELECT COUNT(DISTINCT a1_.id) AS sclr_8 FROM users u2_, auctions a1_ WHERE a1_.by_user_id = ?) AS sclr_7, (SELECT COUNT(DISTINCT o3_.id) AS sclr_10 FROM users u4_, offers o3_ WHERE o3_.by_user_id = ?) AS sclr_9, (SELECT COUNT(DISTINCT o5_.auction_id) AS sclr_12 FROM users u6_, offers o5_ WHERE o5_.by_user_id = ?) AS sclr_11, (SELECT COUNT(DISTINCT o7_.id) AS sclr_14 FROM users u8_, offers o7_ WHERE o7_.value = (SELECT MAX(o9_.value) AS sclr_15 FROM users u10_, offers o9_ WHERE o9_.auction_id = o7_.auction_id) AND o7_.by_user_id = ?) AS sclr_13 FROM users u0_ WHERE u0_.id = ? 
+    
+    NEED TO ELIMINATE USERS SELECT FROM EACH SELECE, CAUSED BY CREATEQUERYBUILDER 'LITERAL'*/
+
     public function findUserprofileInfoCollection($user)
     {
-    $expr = $this->_em->getExpressionBuilder();
+    //$expr = $this->_em->getExpressionBuilder();
        return $this->createQueryBuilder('u')
 
             ->addSelect('('.$this->createQueryBuilder('z')
@@ -112,7 +117,7 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
             ->where('e.Value=('.$this->createQueryBuilder('b')
                 ->select('MAX(r.Value)')
                 ->from('App\Entity\Offer', 'r')
-                ->where('r=e')
+                ->where('r.auction=e.auction')
                 ->getDQL().')')
 
             ->andWhere('e.byUser =  :val')
