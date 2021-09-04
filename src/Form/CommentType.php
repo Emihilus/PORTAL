9994e89ffa347ff61e47ef005d091c1b95d42232
Ernest Mazur <emazemhs@gmail.com>
2021-09-04 14:34:51 +0200

    AUTOCOMMIT: 04-09-2021_14:34:51

---
+++ src/Form/CommentType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Comment;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('content')
+            ->add('createdAt')
+            ->add('modifiedAt')
+            ->add('value')
+            ->add('auction')
+            ->add('user')
+            ->add('byUser')
+            ->add('likedBy')
+            ->add('dislikedBy')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Comment::class,
+        ]);
+    }
+}
