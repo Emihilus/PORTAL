e88e67ac11699e5183cc1a583ac796c67c925c5d
Ernest Mazur <emazemhs@gmail.com>
2021-09-10 15:48:57 +0200

    AUTOCOMMIT: 10-09-2021_15:48:57

--- src/Controller/Security/RegistrationController.php
+++ src/Controller/Security/RegistrationController.php
@@ -34,30 +34,38 @@ class RegistrationController extends AbstractController
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            // encode the plain password
-            $user->setPassword(
-                $passwordEncoder->hashPassword(
-                    $user,
-                    $form->get('plainPassword')->getData()
-                )
-            );
+        if ($form->isSubmitted()) 
+        {
+            if($form->isValid())
+            {
+                // encode the plain password
+                $user->setPassword(
+                    $passwordEncoder->hashPassword(
+                        $user,
+                        $form->get('plainPassword')->getData()
+                    )
+                );
 
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($user);
-            $entityManager->flush();
+                $entityManager = $this->getDoctrine()->getManager();
+                $entityManager->persist($user);
+                $entityManager->flush();
 
-            // generate a signed url and email it to the user
-            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
-                (new TemplatedEmail())
-                    ->from(new Address('system@emazemhs.com', 'PDAPW.PL'))
-                    ->to($user->getEmail())
-                    ->subject('Please Confirm your Email')
-                    ->htmlTemplate('parts/mail/confirmation_email.html.twig')
-            );
-            // do anything else you need here, like send an email
+                // generate a signed url and email it to the user
+                $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
+                    (new TemplatedEmail())
+                        ->from(new Address('system@emazemhs.com', 'PDAPW.PL'))
+                        ->to($user->getEmail())
+                        ->subject('Please Confirm your Email')
+                        ->htmlTemplate('parts/mail/confirmation_email.html.twig')
+                );
+                // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('root');
+                return $this->redirectToRoute('root');
+            }
+            else
+            {
+                $this->addFlash('danger', 'Błąd');
+            }
         }
 
         return $this->render('security/register.html.twig', [
