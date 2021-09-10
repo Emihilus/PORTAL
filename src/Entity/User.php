628e668bab8d65d52ab21dc1ccd83377473a393d
Ernest Mazur <emazemhs@gmail.com>
2021-09-10 15:59:08 +0200

    AUTOCOMMIT: 10-09-2021_15:59:08

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -13,7 +13,6 @@ use Symfony\Component\Security\Core\User\UserInterface;
 /**
  * @ORM\Table(name="users")
  * @ORM\Entity(repositoryClass=UserRepository::class)
- * @UniqueEntity(fields={"username"}, message="There is already an account with this username")
  * @UniqueEntity(fields={"Email"}, message="There is already an account with this email")
  */
 class User implements UserInterface, PasswordAuthenticatedUserInterface
