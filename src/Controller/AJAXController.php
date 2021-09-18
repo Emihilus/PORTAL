c364d7c184ad4f8ba13c7f61327ab81a50ecd56f
Ernest Mazur <emazemhs@gmail.com>
2021-09-18 14:17:51 +0200

    AUTOCOMMIT: 18-09-2021_14:17:51

--- src/Controller/AJAXController.php
+++ src/Controller/AJAXController.php
@@ -660,6 +660,8 @@ class AJAXController extends AbstractController
     }
     // DELETE (SET ISDELETE TO TRUE) ONLY FOR ADMIN AJAX CALL
 
+
+    // EDIT COMMENT ONLY FOR USERS 
     /**
      * @Route("/editComment", name="editComment", methods={"POST"})
      * @Security("is_granted('ROLE_USER')")
@@ -691,9 +693,10 @@ class AJAXController extends AbstractController
             'result' => $result
         ]);
     }
+    // EDIT COMMENT ONLY FOR USERS 
 
 
-
+    // LIKE COMMENT AJAX CALL
     /**
      * @Route("/likeComment", name="likeComment", methods={"POST"})
      */
@@ -728,8 +731,10 @@ class AJAXController extends AbstractController
             ]);
         }
     }
+    // LIKE COMMENT AJAX CALL
 
 
+    // DISLIKE COMMENT AJAX CALL
     /**
      * @Route("/dislikeComment", name="dislikeComment", methods={"POST"})
      */
@@ -764,195 +769,9 @@ class AJAXController extends AbstractController
             ]);
         }
     }
+    // DISLIKE COMMENT AJAX CALL
 
 
-    // CRON TASK
-
-    /**
-     * @Route("/cw", name="cronW")
-     */
-    public function cronW()
-    {
-        $em = $this->getDoctrine()->getManager();
-
-        $this->deleteOldTempImages($em);
-
-        // QUERY BUILDER DESIGN - 32 QUERIES
-        /*$qb = $em->createQueryBuilder();
-        $qb->select('a')
-        ->from('App\Entity\Auction', 'a')
-        ->where('a.endsAt < :now')
-        ->andWhere('a.notificationHandled = false')
-        ->leftJoin('a.offers', 'o')
-        ->leftJoin('o.byUser', 'u')
-        ->setParameter('now', new DateTime());
-        $auctions = $qb->getQuery()->getResult();*/
-
-        // QUERY BUILDER FOREACH
-        /* foreach ($auctions as $auction) 
-        {
-            $winNotification = new Notification();
-
-            $hghstOffer = null;
-            foreach ($auction->getOffer() as $offer) 
-            {
-                if($hghstOffer == null || $offer->getValue() > $hghstOffer->getValue())
-                    $hghstOffer = $offer;
-            }
-            $winNotification->setRecipientUser($hghstOffer->getByUser());
-            $winNotification->setMessage('Wygrales aukcje '.$auction->getTitle());
-            $em->persist($winNotification);
-        }*/
-
-
-        $dql = "SELECT a,
-        (SELECT zu.id
-        FROM App\Entity\Offer zo
-        LEFT JOIN App\Entity\User zu WITH zo.byUser=zu
-        WHERE zo.auction=a
-        AND zo.Value=(SELECT MAX(fo.Value) FROM App\Entity\Offer fo WHERE fo.auction=zo.auction)
-        ) as hghstOfferOwner
-        
-        FROM App\Entity\Offer o
-        
-        LEFT JOIN App\Entity\Auction a WITH a=o.auction
-        
-        WHERE a.endsAt < ?1 
-        AND a.notificationHandled = false";
-
-        $auctions = $em->createQuery($dql)
-        ->setParameter(1,new DateTime())->getResult();
-
-
-        foreach ($auctions as $auction) 
-        {
-            $winNotification = new Notification();
-
-            $winNotification->setRecipientUser($em->getReference('App\Entity\User', $auction['hghstOfferOwner']));
-            $winNotification->setMessage('Wygrales aukcje '.$auction[0]->getTitle());
-            $winNotification->setRelatedEntity(['auction' => $auction[0]->getId()]);
-            $em->persist($winNotification);
-
-            $sellNotification = new Notification();
-            $sellNotification->setRecipientUser($auction[0]->getByUser());
-            $sellNotification->setMessage('Sprzedałeś aukcje '.$auction[0]->getTitle());
-            $sellNotification->setRelatedEntity(['auction' => $auction[0]->getId()]);
-            $em->persist($sellNotification);
-
-            $auction[0]->setNotificationHandled(true);
-            $em->persist($auction[0]);
-        }
-
-
-        
-        // BUYER COMMENT NOTIFICATION
-
-        //  QUERYBUILDER DESIGN
-        /*$qb = $em->createQueryBuilder();
-        $qb->select('c')
-        ->from('App\Entity\Comment', 'c')
-        ->leftJoin('c.auction', 'a')
-        ->where('c.notificationHandled = false')
-        ->andWhere($qb->expr()->neq('c.value', -2));
-        $comments = $qb->getQuery()->getResult();
-
-        foreach ($comments as $comment) 
-        {
-            $buyerCommentNotification = new Notification();
-            $buyerCommentNotification->setRecipientUser($comment->getAuction()->getByUser());
-            $buyerCommentNotification->setMessage('Otrzymałeś komenatrz sprzedaży dot aukcji '.$comment->getAuction()->getTitle());
-            $em->persist($buyerCommentNotification);
-        }*/
-
-        $dql = "SELECT u.id, a.title, c, u.username, c.value
-        
-        FROM App\Entity\User u
-        
-        LEFT JOIN App\Entity\Auction a WITH u=a.byUser
-        LEFT JOIN App\Entity\Comment c WITH a=c.auction
-        
-        WHERE c.value > -2 
-        AND c.notificationHandled = false";
-
-        $comments = $em->createQuery($dql)->getResult();
-
-        foreach ($comments as $comment) 
-        {
-            $buyerCommentNotification = new Notification();
-
-            if($comment['value'] > 1)
-            {
-                $buyerCommentNotification->setRecipientUser($comment[0]->getReplyTo()->getByUser());
-                $buyerCommentNotification->setMessage('Sprzedwca '.$comment['username'].' odpowiedział na Twój komentarz dotyczący aukcji '.$comment['title']);
-                $buyerCommentNotification->setRelatedEntity(['username' => $comment['username']]);
-            }
-            else
-            {
-                $buyerCommentNotification->setRecipientUser($em->getReference('App\Entity\User', $comment['id']));
-                $buyerCommentNotification->setMessage('Otrzymałeś komenatrz sprzedaży dot aukcji '.$comment['title']);
-                $buyerCommentNotification->setRelatedEntity(['username' => $comment['username']]);
-            }
-            $em->persist($buyerCommentNotification);
-
-            $comment[0]->setNotificationHandled(true);
-            $em->persist($comment[0]);
-        }
-
-
-        // REMOVE OLD NOTIFICATONS
-        $now = new DateTime('-1 hour');
-
-        $qb = $em->createQueryBuilder();
-        $qb->select('n')
-        ->from('App\Entity\Notification', 'n')
-        ->where($qb->expr()->lt('n.seenAt', ':date'))
-        ->setParameter('date', $now);
-        $notifications = $qb->getQuery()->getResult();
-
-        foreach ($notifications as $notification) 
-        {
-            $em->remove($notification);
-        }
-        $em->flush();
-
-        dump($notifications);
-        dump($auctions);
-        dump($comments);
-
-
-
-        return $this->render('z_not_used/tst.twig');
-    }
-
-    private function deleteOldTempImages($em)
-    {
-        $tempImages = $em->getRepository(TempImage::class)->findAll();
-        $now = new DateTime();
-
-        foreach ($tempImages as $tempImage) 
-        {
-            
-            if($now->getTimestamp() - $tempImage->getCreatedAt()->getTimestamp()> 600)
-            {
-                try
-                {
-                    $result = unlink($this->getParameter('tempImagePath').$tempImage->getFilename());
-                }
-                catch (\Exception $e)
-                {
-                    if($e->getCode() == 0)
-                        $result = true;
-                }
-                
-                if($result)
-                {
-                    $em->remove($tempImage);
-                }
-                
-            }
-        }
-        $em->flush();
-    }
 
     /**
      * @Route("/get-notifications", name="get-notifications")
---
+++ src/Controller/CRONTasksController.php
@@ -0,0 +1,201 @@
+<?php
+
+namespace App\Controller;
+
+use DateTime;
+use App\Entity\TempImage;
+use App\Entity\Notification;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class CRONTasksController extends AbstractController
+{
+    // CRON TASK
+    /**
+     * @Route("/ep/cw", name="cronW")
+     */
+    public function cronW()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $this->deleteOldTempImages($em);
+
+        // QUERY BUILDER DESIGN - 32 QUERIES
+        /*$qb = $em->createQueryBuilder();
+        $qb->select('a')
+        ->from('App\Entity\Auction', 'a')
+        ->where('a.endsAt < :now')
+        ->andWhere('a.notificationHandled = false')
+        ->leftJoin('a.offers', 'o')
+        ->leftJoin('o.byUser', 'u')
+        ->setParameter('now', new DateTime());
+        $auctions = $qb->getQuery()->getResult();*/
+
+        // QUERY BUILDER FOREACH
+        /* foreach ($auctions as $auction) 
+        {
+            $winNotification = new Notification();
+
+            $hghstOffer = null;
+            foreach ($auction->getOffer() as $offer) 
+            {
+                if($hghstOffer == null || $offer->getValue() > $hghstOffer->getValue())
+                    $hghstOffer = $offer;
+            }
+            $winNotification->setRecipientUser($hghstOffer->getByUser());
+            $winNotification->setMessage('Wygrales aukcje '.$auction->getTitle());
+            $em->persist($winNotification);
+        }*/
+
+
+        $dql = "SELECT a,
+        (SELECT zu.id
+        FROM App\Entity\Offer zo
+        LEFT JOIN App\Entity\User zu WITH zo.byUser=zu
+        WHERE zo.auction=a
+        AND zo.Value=(SELECT MAX(fo.Value) FROM App\Entity\Offer fo WHERE fo.auction=zo.auction)
+        ) as hghstOfferOwner
+        
+        FROM App\Entity\Offer o
+        
+        LEFT JOIN App\Entity\Auction a WITH a=o.auction
+        
+        WHERE a.endsAt < ?1 
+        AND a.notificationHandled = false";
+
+        $auctions = $em->createQuery($dql)
+        ->setParameter(1,new DateTime())->getResult();
+
+
+        foreach ($auctions as $auction) 
+        {
+            $winNotification = new Notification();
+
+            $winNotification->setRecipientUser($em->getReference('App\Entity\User', $auction['hghstOfferOwner']));
+            $winNotification->setMessage('Wygrales aukcje '.$auction[0]->getTitle());
+            $winNotification->setRelatedEntity(['auction' => $auction[0]->getId()]);
+            $em->persist($winNotification);
+
+            $sellNotification = new Notification();
+            $sellNotification->setRecipientUser($auction[0]->getByUser());
+            $sellNotification->setMessage('Sprzedałeś aukcje '.$auction[0]->getTitle());
+            $sellNotification->setRelatedEntity(['auction' => $auction[0]->getId()]);
+            $em->persist($sellNotification);
+
+            $auction[0]->setNotificationHandled(true);
+            $em->persist($auction[0]);
+        }
+
+
+        
+        // BUYER COMMENT NOTIFICATION
+
+        //  QUERYBUILDER DESIGN
+        /*$qb = $em->createQueryBuilder();
+        $qb->select('c')
+        ->from('App\Entity\Comment', 'c')
+        ->leftJoin('c.auction', 'a')
+        ->where('c.notificationHandled = false')
+        ->andWhere($qb->expr()->neq('c.value', -2));
+        $comments = $qb->getQuery()->getResult();
+
+        foreach ($comments as $comment) 
+        {
+            $buyerCommentNotification = new Notification();
+            $buyerCommentNotification->setRecipientUser($comment->getAuction()->getByUser());
+            $buyerCommentNotification->setMessage('Otrzymałeś komenatrz sprzedaży dot aukcji '.$comment->getAuction()->getTitle());
+            $em->persist($buyerCommentNotification);
+        }*/
+
+        $dql = "SELECT u.id, a.title, c, u.username, c.value
+        
+        FROM App\Entity\User u
+        
+        LEFT JOIN App\Entity\Auction a WITH u=a.byUser
+        LEFT JOIN App\Entity\Comment c WITH a=c.auction
+        
+        WHERE c.value > -2 
+        AND c.notificationHandled = false";
+
+        $comments = $em->createQuery($dql)->getResult();
+
+        foreach ($comments as $comment) 
+        {
+            $buyerCommentNotification = new Notification();
+
+            if($comment['value'] > 1)
+            {
+                $buyerCommentNotification->setRecipientUser($comment[0]->getReplyTo()->getByUser());
+                $buyerCommentNotification->setMessage('Sprzedwca '.$comment['username'].' odpowiedział na Twój komentarz dotyczący aukcji '.$comment['title']);
+                $buyerCommentNotification->setRelatedEntity(['username' => $comment['username']]);
+            }
+            else
+            {
+                $buyerCommentNotification->setRecipientUser($em->getReference('App\Entity\User', $comment['id']));
+                $buyerCommentNotification->setMessage('Otrzymałeś komenatrz sprzedaży dot aukcji '.$comment['title']);
+                $buyerCommentNotification->setRelatedEntity(['username' => $comment['username']]);
+            }
+            $em->persist($buyerCommentNotification);
+
+            $comment[0]->setNotificationHandled(true);
+            $em->persist($comment[0]);
+        }
+
+
+        // REMOVE OLD NOTIFICATONS
+        $now = new DateTime('-1 hour');
+
+        $qb = $em->createQueryBuilder();
+        $qb->select('n')
+        ->from('App\Entity\Notification', 'n')
+        ->where($qb->expr()->lt('n.seenAt', ':date'))
+        ->setParameter('date', $now);
+        $notifications = $qb->getQuery()->getResult();
+
+        foreach ($notifications as $notification) 
+        {
+            $em->remove($notification);
+        }
+        $em->flush();
+
+        // dump($notifications);
+        // dump($auctions);
+        // dump($comments);
+
+
+
+        return $this->render('z_not_used/tst.twig');
+        // return new Response("Done");
+    }
+
+    private function deleteOldTempImages($em)
+    {
+        $tempImages = $em->getRepository(TempImage::class)->findAll();
+        $now = new DateTime();
+
+        foreach ($tempImages as $tempImage) 
+        {
+            
+            if($now->getTimestamp() - $tempImage->getCreatedAt()->getTimestamp()> 600)
+            {
+                try
+                {
+                    $result = unlink($this->getParameter('tempImagePath').$tempImage->getFilename());
+                }
+                catch (\Exception $e)
+                {
+                    if($e->getCode() == 0)
+                        $result = true;
+                }
+                
+                if($result)
+                {
+                    $em->remove($tempImage);
+                }
+                
+            }
+        }
+        $em->flush();
+    }
+}
